f33ad0a80e460e79421e8ac5f6818c8d15924e02
Matt Hannah <matt.hannah@daisee.com>
2018-11-17 16:10:11 +1100

    Add home pc config

---
+++ home-pc/configuration.nix
@@ -0,0 +1,104 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda3";
+      preLVM = true;
+    }
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    coreutils
+    vim
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the Desktop Environment.
+  services.xserver.desktopManager = {
+    default = "xfce";
+    xfce.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.matt = {
+    createHome = true;
+    extraGroups = ["wheel" "video" "audio" "disk" "networkmanager"];
+    group = "users";
+    home = "/home/matt";
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    uid = 1000;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
---
+++ home-pc/home.nix
@@ -0,0 +1,59 @@
+{config, pkgs, ...}:
+
+{
+  home.packages = with pkgs; [
+    audacity
+    curl
+    git
+    git-crypt
+    gnumake
+    gnupg
+    google-chrome
+    htop
+    networkmanagerapplet
+    oh-my-zsh
+    postgresql
+    slack
+    terminator
+    unzip
+    vlc
+    vscode
+    xscreensaver
+    zip
+  ];
+
+
+  programs.zsh.enable = true;
+
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    plugins = [
+      "git"
+    ];
+    theme = "robbyrussell";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Matt Hannah";
+    userEmail = "matt.hannah@daisee.com";
+    aliases.lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
+
+  services.redshift = {
+    enable = true;
+    latitude = "-37.7688";
+    longitude = "145.0423";
+    tray = false;
+  };
+
+  services.unclutter.enable = true;
+
+  programs.home-manager.enable = true;
+  programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+}
